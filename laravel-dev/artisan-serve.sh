5b455e8b7700b79a5b6b2bd8b65a19ae981add4b
PastLeo <chgu82837@gmail.com>
2015-04-11 19:41:58 +0800

    add cplusplus, python, slideshow template

--- laravel-dev/artisan-serve.sh
+++ laravel-dev/artisan-serve.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd $(dirname $0)
 echo "Starting the laravel self-built development server on http://0.0.0.0:80 ..."
-php /laravel/artisan serve --host=0.0.0.0 --port=80 &
+php ./artisan serve --host=0.0.0.0 --port=80 &
 echo "You can still use command now like composer dump, php artisan migrate ..."
 echo "enter 'exit' to exit and close the dev env"
 bash
