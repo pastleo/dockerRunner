cb30c7603496a7f25000faebb03636df0450c769
PastLeo <chgu82837@gmail.com>
2015-04-16 21:50:40 +0800

    now laravel works perfectly

--- laravel-dev/artisan-serve.sh
+++ laravel-dev/artisan-serve.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd /workspace
 echo "Starting the laravel self-built development server on http://0.0.0.0:80 ..."
-php ./artisan serve --host=0.0.0.0 --port=80 &
-echo "You can still use command now like composer dump, php artisan migrate ..."
-echo "enter 'exit' to exit and close the dev env"
+if ! [ -f ./artisan ]; then 
+    echo "No artisan found. Create an empty project? [Y/n]"
+    read answer
+    case $answer in
+    "Y")
+        echo "Creating ..."
+        composer create-project laravel/laravel --prefer-dist ./ 
+        ;;
+    *)
+        echo "Use 'composer create-project laravel/laravel --prefer-dist ./' to create an empty laravel here."
+        ;;
+    esac
+fi
+
+if [ -f ./artisan ]; then 
+    php ./artisan || /usr/local/bin/composer update
+    php ./artisan serve --host=0.0.0.0 --port=80 &
+    echo "You can use command like composer dump, php artisan migrate here."
+fi
+
+sleep 5
+echo "This is an bash interactive environment with composer, php56."
+echo "enter 'exit' to exit and close the dev env."
 bash
