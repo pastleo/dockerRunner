4633cf6afe6dfc1d73fc84948266b10fb7fcb6b6
PastLeo <chgu82837@gmail.com>
2015-07-24 18:18:08 +0800

    add rails env

---
+++ ruby_or_rails/run.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+pkgFile="Gemfile"
+tmpl="ruby_or_rails-tmpl"
+installAct="bundle"
+manInstall="gem install ..."
+
+echo "==============================="
+
+if [ -f "$pkgFile" ]; then
+	echo "'$pkgFile' exists, install requirements for development evnironment ..."
+
+	eval $installAct
+
+    echo ""
+	echo "To avoid installing package every time you run this env, use a template dockeRun env '$tmpl' by"
+	echo "    dockeRun --template $tmpl"
+else
+	echo "'$pkgFile' not exists,"
+	echo "You need to install packages by '$manInstall' on your own"
+	echo "Or put '$pkgFile' in the root of the working directory and rerun the container to get package installed automatically."
+	echo "Or use a template dockeRun env '$tmpl' by"
+	echo "    dockeRun --template $tmpl"
+fi
+echo ""
+
+/bin/bash
